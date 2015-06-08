60c6d2659789684b7700753b9b893291f9a53983
Madhur Bhargava <madhur.bhargava@claricetechnologies.com>
2015-06-08 16:37:40 +0530

    UI changes to list item
    Multiple items added to list view layout

--- gen/com/example/androidlistviewmultithreading/R.java
+++ gen/com/example/androidlistviewmultithreading/R.java
@@ -1632,8 +1632,10 @@ containing a value of this type.
         public static final int abc_textfield_search_activated_mtrl_alpha=0x7f020030;
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020031;
         public static final int abc_textfield_search_material=0x7f020032;
-        public static final int ic_launcher=0x7f020033;
-        public static final int loading=0x7f020034;
+        public static final int border=0x7f020033;
+        public static final int ic_launcher=0x7f020034;
+        public static final int loading=0x7f020035;
+        public static final int ratings=0x7f020036;
     }
     public static final class id {
         public static final int action_bar=0x7f090031;
@@ -1649,7 +1651,7 @@ containing a value of this type.
         public static final int action_mode_bar=0x7f09002e;
         public static final int action_mode_bar_stub=0x7f09002d;
         public static final int action_mode_close_button=0x7f090020;
-        public static final int action_settings=0x7f090040;
+        public static final int action_settings=0x7f090045;
         public static final int activity_chooser_view_content=0x7f090021;
         public static final int always=0x7f090014;
         public static final int beginning=0x7f09001b;
@@ -1657,8 +1659,10 @@ containing a value of this type.
         public static final int collapseActionView=0x7f090015;
         public static final int decor_content_parent=0x7f09002f;
         public static final int default_activity_button=0x7f090024;
+        public static final int details=0x7f090040;
         public static final int dialog=0x7f090019;
         public static final int disableHome=0x7f09000d;
+        public static final int distance=0x7f090042;
         public static final int dropdown=0x7f09001a;
         public static final int edit_query=0x7f090033;
         public static final int end=0x7f09001c;
@@ -1669,6 +1673,8 @@ containing a value of this type.
         public static final int icon=0x7f090026;
         public static final int ifRoom=0x7f090016;
         public static final int image=0x7f090023;
+        public static final int img_ratings=0x7f090041;
+        public static final int last_booking=0x7f090043;
         public static final int list=0x7f09003f;
         public static final int listMode=0x7f09000a;
         public static final int list_item=0x7f090025;
@@ -1679,6 +1685,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f090007;
         public static final int progress_horizontal=0x7f090008;
         public static final int radio=0x7f09002b;
+        public static final int rupees=0x7f090044;
         public static final int search_badge=0x7f090035;
         public static final int search_bar=0x7f090034;
         public static final int search_button=0x7f090036;
